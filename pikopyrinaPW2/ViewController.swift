291fef8026a3a5434714b155ddc2410efc561d52
pkopyrina <pkopyrina@gmail.com>
2022-10-10 00:18:55 +0300

    Initial Commit

---
+++ pikopyrinaPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pikopyrinaPW2
+//
+//  Created by Polina Kopyrina on 10.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
